44f1a7b6961b78ecb4ae7a6123d8ede6ea0bdab7
Andres Sainz <andresmsainz@gmail.com>
2021-05-23 12:46:13 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections; //Used for stacks
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks!");
+
+            //Creating
+            Stack stack = new Stack();
+
+            //Adding
+            stack.Push("David");
+            stack.Push("Emmy");
+            stack.Push("Jack");
+            stack.Push("Emma");
+
+            //Removing
+            Console.WriteLine("Removing " + stack.Pop());
+            Console.WriteLine("Removing " + stack.Pop());
+            Console.WriteLine("Removing " + stack.Pop());
+            Console.WriteLine("Removing " + stack.Pop());
+
+        }
+    }
+}
